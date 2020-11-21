e3b64dd513a792a29ca89243e47f45b46305c983
ilhamaf53 <ilham.ahmadfahrizal@gmail.com>
2020-11-21 14:16:30 +0700

    perubahan pertama

--- admin.php
+++ admin.php
@@ -1,12 +1,13 @@
 <?php
-require_once"core/init.php";
+require_once "core/init.php";
 
-if(!isset($_SESSION['user'])){
+if (!isset($_SESSION['user'])) {
   header('Location:index.php');
-}   
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -22,6 +23,7 @@ if(!isset($_SESSION['user'])){
   <!-- GOOGLE FONTS-->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
+
 <body>
   <div id="wrapper">
     <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
@@ -32,14 +34,14 @@ if(!isset($_SESSION['user'])){
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="admin.php">Wedding Organizer</a> 
+        <a class="navbar-brand" href="admin.php">Wedding Organizer</a>
       </div>
       <div style="color: white;
       padding: 15px 50px 5px 50px;
       float: right;
       font-size: 16px;"> Terakhir Akses : <?php date_default_timezone_set('Asia/Jakarta');
-      echo date('d-m-Y H:i:s');?>  &nbsp; <a href="logout.php" class="btn btn-danger square-btn-adjust">Keluar</a> </div>
-    </nav>   
+                                          echo date('d-m-Y H:i:s'); ?> &nbsp; <a href="logout.php" class="btn btn-danger square-btn-adjust">Keluar</a> </div>
+    </nav>
     <!-- /. NAV TOP  -->
     <nav class="navbar-default navbar-side" role="navigation">
       <div class="sidebar-collapse">
@@ -47,54 +49,64 @@ if(!isset($_SESSION['user'])){
           <li class="text-center">
           </li>
           <li>
-            <a class="active-menu"  href="admin.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
+            <a class="active-menu" href="admin.php?halaman=home.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
           </li>
           <li>
-            <a  href="#"><i class="fa fa-newspaper-o fa-3x"></i> Paket Pre-Wedding </a>
-          </li> 
+            <a href="admin.php?halaman=paket-prewed"><i class="fa fa-newspaper-o fa-3x"></i> Paket Pre-Wedding </a>
+          </li>
           <li>
-            <a  href="#"><i class="fa fa-newspaper-o fa-3x"></i> Paket Catering </a>
-          </li> 
+            <a href="#"><i class="fa fa-newspaper-o fa-3x"></i> Paket Catering </a>
+          </li>
           <li>
-            <a  href="#"><i class="fa fa-newspaper-o fa-3x"></i> Paket Gaun </a>
-          </li> 
+            <a href="admin.php?halaman=paket-gaun"><i class="fa fa-newspaper-o fa-3x"></i> Paket Gaun </a>
+          </li>
           <li>
-            <a  href="#"><i class="fa fa-newspaper-o fa-3x"></i> Paket Gedung </a>
-          </li> 
+            <a href="#"><i class="fa fa-newspaper-o fa-3x"></i> Paket Gedung </a>
+          </li>
           <li>
-            <a  href="#"><i class="fa fa-newspaper-o fa-3x"></i> Partner </a>
-          </li> 
+            <a href="#"><i class="fa fa-newspaper-o fa-3x"></i> Partner </a>
+          </li>
         </ul>
       </div>
-    </nav>  
+    </nav>
     <!-- /. NAV SIDE  -->
-    <div id="page-wrapper" >
+    <div id="page-wrapper">
       <div id="page-inner">
         <div class="row">
           <div class="col-md-12">
-           <h2>Admin Dashboard</h2>   
-           <h5>Selamat Datang Admin, Semoga Harimu Menyenangkan. </h5>
-         </div>
-       </div>              
-       <!-- /. ROW  -->
-       <hr />
-       <!-- /. PAGE INNER  -->
-     </div>
-     <!-- /. PAGE WRAPPER  -->
-   </div>
- </div>
- <!-- /. WRAPPER  -->
- <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
- <!-- JQUERY SCRIPTS -->
- <script src="assets/js/jquery-1.10.2.js"></script>
- <!-- BOOTSTRAP SCRIPTS -->
- <script src="assets/js/bootstrap.min.js"></script>
- <!-- METISMENU SCRIPTS -->
- <script src="assets/js/jquery.metisMenu.js"></script>
- <!-- MORRIS CHART SCRIPTS -->
- <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
- <script src="assets/js/morris/morris.js"></script>
- <!-- CUSTOM SCRIPTS -->
- <script src="assets/js/custom.js"></script>
+            <?php
+            if (isset($_GET['halaman'])) {
+              if ($_GET['halaman'] == "paket-prewed") {
+                include 'paket-prewed.php';
+              } elseif ($_GET['halaman'] == "paket-gaun") {
+                include 'paket-gaun.php';
+              } else {
+                include 'home.php';
+              }
+            }
+            ?>
+          </div>
+        </div>
+        <!-- /. ROW  -->
+        <hr />
+        <!-- /. PAGE INNER  -->
+      </div>
+      <!-- /. PAGE WRAPPER  -->
+    </div>
+  </div>
+  <!-- /. WRAPPER  -->
+  <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+  <!-- JQUERY SCRIPTS -->
+  <script src="assets/js/jquery-1.10.2.js"></script>
+  <!-- BOOTSTRAP SCRIPTS -->
+  <script src="assets/js/bootstrap.min.js"></script>
+  <!-- METISMENU SCRIPTS -->
+  <script src="assets/js/jquery.metisMenu.js"></script>
+  <!-- MORRIS CHART SCRIPTS -->
+  <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+  <script src="assets/js/morris/morris.js"></script>
+  <!-- CUSTOM SCRIPTS -->
+  <script src="assets/js/custom.js"></script>
 </body>
+
 </html>
--- functions/func-admin.php
+++ functions/func-admin.php
@@ -1,37 +1,39 @@
 <?php
-    function cek_data_name($nama,$pass){
-        global $link;
-        $query="SELECT username FROM admin WHERE password='$pass'";
-        if($result=mysqli_query($link,$query)){
-            if(mysqli_num_rows($result)!=0)return true;
-                else return false;
-            }
+function cek_data_name($nama, $pass)
+{
+    global $link;
+    $query = "SELECT username FROM admin WHERE password='$pass'";
+    if ($result = mysqli_query($link, $query)) {
+        if (mysqli_num_rows($result) != 0) return true;
+        else return false;
     }
-    function cek_data_pass($nama,$pass){
-        global $link;
-        $query="SELECT password FROM admin WHERE username='$nama'";
-        if($result=mysqli_query($link,$query)){
-            if(mysqli_num_rows($result)!=0)return true;
-                else return false;
-            }
+}
+function cek_data_pass($nama, $pass)
+{
+    global $link;
+    $query = "SELECT password FROM admin WHERE username='$nama'";
+    if ($result = mysqli_query($link, $query)) {
+        if (mysqli_num_rows($result) != 0) return true;
+        else return false;
     }
-   function red_login($nama){
-        $_SESSION['user']=$nama;
-        header('Location:admin.php');
-    }
-    function tampil_user($user){
-        global $link;
-        $query = "SELECT * FROM admin WHERE username='$user'";
-        $e = mysqli_query($link, $query);
-        // hitung hasil dan cek ada atau tidaknya data
-        $is_exist = mysqli_num_rows($e);
-        if($is_exist>0){
+}
+function red_login($nama)
+{
+    $_SESSION['user'] = $nama;
+    header('Location:admin.php');
+}
+function tampil_user($user)
+{
+    global $link;
+    $query = "SELECT * FROM admin WHERE username='$user'";
+    $e = mysqli_query($link, $query);
+    // hitung hasil dan cek ada atau tidaknya data
+    $is_exist = mysqli_num_rows($e);
+    if ($is_exist > 0) {
         // keluarkan hasil
         $r = mysqli_fetch_assoc($e);
         $_SESSION['username'] = $r['username']; // set session untuk nama
-        }else{
-    die('username tidak ditemukan');
-}
-
+    } else {
+        die('username tidak ditemukan');
     }
-?>
+}
